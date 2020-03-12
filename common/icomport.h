4f5f72876b58dfb87fce0bae4d5494813f7789d5
Gleb K <49366600+Glebonsky@users.noreply.github.com>
2020-03-13 01:17:23 +0300

    add some defines

--- common/icomport.h
+++ common/icomport.h
@@ -6,7 +6,7 @@
 class IComPort : public Publisher
 {
 public:
-    virtual void sendMessage(const char *) = 0;
+    virtual void sendMessage(const char *msg) = 0;
     virtual const char *readMessage() = 0;
     virtual ~IComPort() {}
 };
--- common/keywords.h
+++ common/keywords.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #define connnectionSpeed 115200
+#define maxMessageLength 25
 
 #define movingCamId "MOVING_CAM"
 #define connectRequest "CONNECT_TO_MOVING_CAM"
@@ -10,3 +11,5 @@
 //Command names
 #define currentPosition "CURRENT_POS"
 #define moveTo "MOVE"
+// #define moveX "MOVE_X"
+// #define 
