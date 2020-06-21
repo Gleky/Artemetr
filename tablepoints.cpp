2b3fa631524fe803e74ed1e1636d0a43a0799bbf
Gleb K <49366600+Glebonsky@users.noreply.github.com>
2020-06-22 02:25:23 +0300

    target points compute done

--- tablepoints.cpp
+++ tablepoints.cpp
@@ -2,11 +2,14 @@
 
 #include "keywords.h"
 
+QList<Point> getPackList();
+
 TabelPoints::TabelPoints()
 {
-    reset();
-    for (auto unused : _packList) //test
-        nextCheckPoint(); //test
+    _packsForCheck = getPackList();
+
+    while ( hasNextCheckPoint() ) //test
+        currentCheckIs(true);     //test
 }
 
 bool TabelPoints::hasNextCheckPoint() const
@@ -19,17 +22,17 @@ bool TabelPoints::hasNextCheckPoint() const
     return ret;
 }
 
-const Point TabelPoints::nextCheckPoint()
+const Point TabelPoints::nextCheckPoint() const
 {
     Point checkPoint;
 
     if ( !_packsForCheck.isEmpty() )
     {
-        _currentPack = _packsForCheck.takeFirst();
+        auto currentPack = _packsForCheck.first();
         constexpr int gap = (xMaxPos - 2*tableMargin - packWidth*horisontalPackCount)/(horisontalPackCount - 1);
 
-        checkPoint.X = tableMargin + _currentPack.X*(packWidth + gap) + packWidth/2;
-        checkPoint.Y = tableMargin + _currentPack.Y*(packHeight + gap) + packHeight/2;
+        checkPoint.X = tableMargin + currentPack.X*(packWidth + gap) + packWidth/2;
+        checkPoint.Y = tableMargin + currentPack.Y*(packHeight + gap) + packHeight/2;
     }
 
     return checkPoint;
@@ -37,27 +40,58 @@ const Point TabelPoints::nextCheckPoint()
 
 void TabelPoints::currentCheckIs(bool checkResult)
 {
-    if ( !checkResult )
+    if ( _packsForCheck.isEmpty() )
+        return;
+
+    if ( checkResult )
     {
-        _packList.removeOne(_currentPack);
+        _packList.append( _packsForCheck.takeFirst() );
+    }
+    else
+    {
+        _packsForCheck.removeFirst();
+    }
+
+    if ( _packsForCheck.isEmpty() )
+        getTargetPoints();
+}
+
+QList<Point> packTargetPoints(Point packPosition, bool rightToLeftDirection);
+
+void TabelPoints::getTargetPoints()
+{
+    while ( !_packList.isEmpty() )
+    {
+        auto currentPackPosition = _packList.takeLast();
+        bool rightToLeft = currentPackPosition.Y % 2;
+
+        _targetPoints += packTargetPoints(currentPackPosition, rightToLeft);
     }
 }
 
 bool TabelPoints::hasNextTargetPoint() const
 {
     bool ret = false;
+
+    if ( !_targetPoints.isEmpty() )
+        ret = true;
+
     return ret;
 }
 
 Point TabelPoints::nextTargetPoint()
 {
-    Point ret;
-    return ret;
+    return _targetPoints.takeFirst();
 }
 
-void TabelPoints::reset()
+
+
+
+QList<Point> getPackList()
 {
     int step = -1;
+
+    QList<Point> packList;
 
     int currentX = horisontalPackCount;
     int currentY = verticalPackCount;
@@ -68,18 +102,49 @@ void TabelPoints::reset()
         for ( int n = 0; n < horisontalPackCount; ++n)
         {
             currentX += step;
-            _packList.append( Point(currentX,currentY) );
+            packList.append( Point(currentX,currentY) );
         }
         currentX += step;
         step = -step;
     }
 
-    _packsForCheck = _packList;
+    return packList;
 }
 
-void TabelPoints::getTargetPoints()
+QList<Point> packTargetPoints(Point packPosition, bool rightToLeftDirection)
 {
-    //start from end of pack sequence
+    constexpr int gap = (xMaxPos - 2*tableMargin - packWidth*horisontalPackCount)/(horisontalPackCount - 1);
 
-    //find target points depends on pack position
+    int startX = tableMargin + packPosition.X*(packWidth + gap);
+    const int startY = tableMargin + packPosition.Y*(packHeight + gap);
+
+    double xStep =  static_cast<double>(packWidth)/(horisontalCellCount - 1);
+    double yStep = static_cast<double>(packHeight)/(verticalCellCount - 1);
+
+    if ( rightToLeftDirection )
+    {
+        startX += packWidth;
+        xStep = -xStep;
+    }
+    const Point startPoint(startX,startY);
+
+    QList<Point> targetPoints;
+
+    for ( int m = 0; m < horisontalCellCount; ++m)
+    {
+        int x = startPoint.X + lround(xStep*m);
+
+        int localStartY = startPoint.Y;
+        if ( yStep < 0 )
+            localStartY += packHeight;
+
+        for ( int n = 0; n < verticalCellCount; ++n)
+        {
+            int y = localStartY + lround(yStep*n);
+            targetPoints.append( Point(x,y) );
+        }
+        yStep = -yStep;
+    }
+
+    return targetPoints;
 }
--- tablepoints.h
+++ tablepoints.h
@@ -11,17 +11,15 @@ public:
     TabelPoints();
 
     bool hasNextCheckPoint() const;
-    const Point nextCheckPoint();
+    const Point nextCheckPoint() const;
     void currentCheckIs( bool checkResult );
 
     bool hasNextTargetPoint() const;
     Point nextTargetPoint();
 
-    void reset();
-
 private:
+//    void getPackList();
     QList<Point> _packList;
-    Point _currentPack;
 
     QList<Point> _packsForCheck;
 
