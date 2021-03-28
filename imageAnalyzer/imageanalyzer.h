51cf905ee44c0ce9a7ea6a245f47507cc09f1235
Gleb K <glebgkom@gmail.com>
2021-03-28 13:13:45 +0300

    Removed objects counting
    Neuro network does it now

--- imageAnalyzer/imageanalyzer.h
+++ imageAnalyzer/imageanalyzer.h
@@ -4,7 +4,6 @@
 #include "imageAnalyzer_global.h"
 
 #include <QObject>
-#include "../struct_result.h"
 
 
 class IMAGEANALYZER_EXPORT ImageAnalyzer : public QObject
@@ -14,17 +13,12 @@ public:
     ImageAnalyzer();
 
     void checkPackPresence(const QImage &img);
-    void getResult(const QImage &img);
 
 signals:
     void packPresence( bool presence );
-    void resultReady(CellResult);
 
 private:
     void checkPresence(QImage img);
-    void computeResult(QImage img);
 };
-
-Q_DECLARE_METATYPE(CellResult)
 
 #endif // IMAGEANALYZER_H
